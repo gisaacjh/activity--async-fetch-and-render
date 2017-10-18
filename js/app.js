0d9a0d059d6799c8cd9b43d49aa3b70f4185359f
Isaac <gisaacjh@outlook.com>
2017-10-18 12:50:57 -0500

    done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
 const request = superagent
+var fetch = document.querySelector('button').addEventListener('click', function () {
+
+
+
+var personas = document.querySelector('.users-list')
+
+request
+
+.get('https://randomuser.me/api/?results=50')
+.then(function savePersonsInAVariable (data) {
+      const persons = data.body.results
+
+      console.log(persons);
+
+      personas.innerHTML = ''
+
+persons.forEach(function(obj) {
+            console.log(obj.email);
+            var nation = obj.nat
+            var eMail = obj.email
+            personas.innerHTML += `
+              <article>
+                <span>${nation}</span>
+                <h6>${eMail}</h6>
+              </article>`
+            })
+          })
+})
